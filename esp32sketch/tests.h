aff0b887af1c259f2a689b2e90308185cf9741c6
NiklasReini <39067921+NiklasReini@users.noreply.github.com>
2021-11-16 15:43:48 +0200

    Update tests.h
    Adding connectivity test

--- esp32sketch/tests.h
+++ esp32sketch/tests.h
@@ -22,6 +22,30 @@ test(example) {
   
 }
 
+test(Connectivity){
+
+  delay(10);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid); 
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password); 
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  assertEqual(WiFi.status(), WL_CONNECTED);
+}
+
 test(thisThing) {
   assertStringCaseEqual("hello", "Hello");
 }
